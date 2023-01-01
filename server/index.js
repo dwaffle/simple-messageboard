756ae1b2246a7063beca8a03f3cc0b7032ddc378
dwaffle <stryderdlw@gmail.com>
2023-01-01 11:54:04 -0800

    Cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", req.headers.origin)
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
